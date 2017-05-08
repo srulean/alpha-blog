18e2b83d84c08beaedbaad79a5a631113ad9062d
tfil Udemy Course <mitofi_@hotmail.com>
2017-05-08 20:08:43 +0000

    Index, Edit, UPdate, SHow.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,21 @@
 class ArticlesController < ApplicationController
   
+  def index
+    @articles = Article.all
+  end
+  
   def new
+    # Need to send in an Article object
     @article = Article.new
   end
   
+  def edit
+    # Need to send in an Article object
+    @article = Article.find(params[:id])
+  end
+  
   def create
+    # Used by the new funciotn above.
     @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was successfully created."
@@ -12,6 +23,18 @@ class ArticlesController < ApplicationController
     else
       #flash[:notice] = "Article was NOT successfully created."
       render 'new'
+    end
+  end
+  
+  def update
+    # Used by the edit funciotn above.
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated."
+      redirect_to article_path(@article)
+    else
+      #flash[:notice] = "Article was NOT successfully created."
+      render 'edit'
     end
   end
   
