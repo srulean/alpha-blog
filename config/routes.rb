4e866655992783a4a203d856bdaf5d1192da1135
tfil Udemy Course <mitofi_@hotmail.com>
2017-05-15 21:44:46 +0000

    SIgn up users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
   get 'home2', to: 'pages#home2'
   
   resources :articles
+  
+  get 'signup', to: 'users#new'
+  # post 'users', to: 'users#create'
+  resources :users, except: [:new]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
