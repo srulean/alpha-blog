6bf3007ada2af6c445334b4bd3e847e585ee718a
tfil Udemy Course <mitofi_@hotmail.com>
2017-05-17 21:15:34 +0000

    Log in - first part

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,10 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   # post 'users', to: 'users#create'
   resources :users, except: [:new]
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'login', to: 'sessions#destroy'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
