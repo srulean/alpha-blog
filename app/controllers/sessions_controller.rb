4281a68f9c836aefbe0a90afebcae81c27763f68
tfil Udemy Course <mitofi_@hotmail.com>
2017-05-22 19:41:35 +0000

    Authenticaiotn complete

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,31 @@ class SessionsController < ApplicationController
   end
   
   def create
-    
+    user = User.find_by(email: params[:session][:email].downcase)
+    # user can be null if email is not found, so check for it.
+    # Also check for password.
+    if user && user.authenticate(params[:session][:password])
+      # Save the user id in the session
+      session[:user_id] = user.id
+      
+      # Notify success
+      flash[:success] = "You have successfully logged in!"
+      
+      # redirects to the user's show page.
+      redirect_to user_path(user)
+      
+    else
+      # Using flash.now (instead of flash) will persist the message (??)
+      flash.now[:danger] = "There was somethign wrong with login credentials."
+      render 'new'
+    end
   end
   
   def destroy
-  
+    # thi sia logout action
+    session[:user_id] = nil
+    flash[:success] = "You have logged out."
+    redirect_to root_path
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  delete 'login', to: 'sessions#destroy'
+  delete 'logout', to: 'sessions#destroy'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
